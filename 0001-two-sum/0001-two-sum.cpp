624d56a51e9d0fe987884c00a371571e012abe4d
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2025-11-24 14:01:28 +0530

    Time: 0 ms (100%), Space: 14.8 MB (52.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int,int> hash;
         for(int i=0;i<nums.size();i++){
-            int findVal = target-nums[i];
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[j] == findVal){
-                    return vector<int>{i,j};
-                }
+            int findVal = (target-nums[i]);
+            auto it = hash.find(findVal);
+            if(it != hash.end()){
+                return vector<int>{hash[findVal], i};
             }
+            hash[nums[i]] = i;
         }
         
     return vector<int>{1,2};
