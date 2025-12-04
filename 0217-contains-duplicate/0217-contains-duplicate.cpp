658d87d108372b163a6bbf9f3d2542d701ef89f9
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2025-12-04 07:55:11 +0530

    Time: 70 ms (18.13%), Space: 93.2 MB (7.33%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,12 +3,10 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> s;
         for(int i=0;i<nums.size();i++){
-            if(s.find(nums[i]) != s.end()){
-                return true;
-            }else{
-                s.insert(nums[i]);
-            }
+            s.insert(nums[i]);
         }
+
+        if(s.size() < nums.size()) return true;
         return false;
     }
 };
