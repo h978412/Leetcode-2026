d9326e9af06d69dfef110c7ebdbb280e0386522a
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2025-12-11 20:25:53 +0530

    Time: 8 ms (27.11%), Space: 24.6 MB (21.62%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,11 +4,11 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
-        for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0] <= ans.back()[1]){
-                ans.back()[1] = max(intervals[i][1],ans.back()[1]);
+        for(auto interval : intervals){
+            if(interval[0] <= ans.back()[1]){
+                ans.back()[1] = max(interval[1],ans.back()[1]);
             }else{
-                ans.push_back(intervals[i]);
+                ans.push_back(interval);
             }
         }
         return ans;
