ec7c8e56b328c560959242c7fa5147d466351d45
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2025-12-11 16:02:04 +0530

    Time: 0 ms (100%), Space: 97.3 MB (60.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,8 +4,8 @@ public:
         int minPrice = prices[0];
         int maxProfit = 0;
         for(int i=1;i<prices.size();i++){
-            int profit = prices[i] - minPrice ;
-            maxProfit = max(profit,maxProfit);
+            // int profit = prices[i] - minPrice ;
+            maxProfit = max(prices[i] - minPrice,maxProfit);
             minPrice = min(minPrice,prices[i]);
         }
         return maxProfit;
