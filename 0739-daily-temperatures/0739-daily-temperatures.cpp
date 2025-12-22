abf58cb999d456a7ca32ad1c08269156559b8c0b
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2025-12-23 01:44:23 +0530

    Time: 19 ms (73.05%), Space: 103 MB (72.94%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& t) {
         int temp[101];
-        vector<int> ans(t.size(),0);
+        // vector<int> ans(t.size(),0);
         stack<int> s;
         for(int i=t.size()-1;i>=0;i--){
-            temp[t[i]] = i;
-            while(!s.empty() && s.top()<= t[i]){
+            int curr = t[i];
+            temp[curr] = i;
+            while(!s.empty() && s.top()<= curr){
                 s.pop();
             }
-            if(s.empty()) ans[i] = 0;
-            else ans[i] = temp[s.top()] - i;
-            s.push(t[i]);
+            if(s.empty()) t[i] = 0;
+            else t[i] = temp[s.top()] - i;
+            s.push(curr);
         }
-        return ans;
+        return t;
         
     }
 };
