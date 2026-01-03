666f7ef7246c3b2a03c3dc860cc58cb1920267d4
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-01-03 13:14:26 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.67%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int numOfWays(int n) {
+        long A = 6;
+        long B = 6;
+        int MOD = 1000000007;
+        for(int i=2;i<=n;i++){
+            long newA = (2*A + 2*B)%MOD;
+            long newB = (2*A + 3*B)%MOD;
+            A = newA;
+            B = newB;
+        }
+
+        return (int)((A+B)%MOD);
+
+    }
+};
