fce006c03edbd4e8ba5b7a4dffd7fd58d8af9745
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-01-07 23:36:31 +0530

    Time: 202 ms (6.05%), Space: 98.4 MB (5.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>m(amount+1,vector<int>(coins.size()+1));
+        vector<vector<int>>m(amount+1,vector<int>(coins.size()+1,-1));
         for(int i=0;i<=coins.size();i++){
             m[0][i] = 0;
         }
-        for(int i=1;i<=amount;i++){
-            m[i][0] = -1;
-        }
-
         for(int i=1;i<=amount;i++){
             for(int j=1;j<=coins.size();j++){
                 int count1 =-1, count2 =-1;
