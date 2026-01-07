db882aa5b5ba335cff6353dbf2bc399427b91f98
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-01-07 22:14:21 +0530

    Time: 227 ms (5.02%), Space: 99.5 MB (5.1%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>>m(amount+1,vector<int>(coins.size()+1,-2));
+        return this->rec(coins,coins.size(),amount,m);
+    }
+    int rec(vector<int>& coins,int i, int amount,vector<vector<int>>& m){
+
+        if(m[amount][i] != -2) return m[amount][i]; 
+
+        if(amount == 0){
+            m[amount][i] = 0;
+            return 0;
+        }
+            
+        if(i == 0){
+            m[amount][i] = -1;
+            return -1;
+        }
+
+        int count1=-1,count2=-1;
+        if(coins[i-1] <= amount){
+            count1 = this->rec(coins,i,(amount-coins[i-1]),m);
+        }
+        count2 = this->rec(coins,i-1,amount,m);
+        if(count1 == -1 && count2 == -1) m[amount][i] = -1;
+        else if(count1 == -1) m[amount][i] =  count2;
+        else if(count2 == -1) m[amount][i] =  count1+1;
+        else m[amount][i] =  min(count1+1,count2);
+        return m[amount][i];
+    }
+};
