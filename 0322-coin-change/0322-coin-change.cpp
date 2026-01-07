dd675a1adfd246bd4351d71c47f73c9088611f28
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-01-07 23:30:40 +0530

    Time: 216 ms (5.45%), Space: 98.3 MB (5.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>m(amount+1,vector<int>(coins.size()+1,-2));
-        return this->rec(coins,coins.size(),amount,m);
-    }
-    int rec(vector<int>& coins,int i, int amount,vector<vector<int>>& m){
-
-        if(m[amount][i] != -2) return m[amount][i]; 
-        if(amount == 0) return 0;
-        if(i == 0) return -1;
-
-        int count1=-1,count2=-1;
-        if(coins[i-1] <= amount){
-            count1 = this->rec(coins,i,(amount-coins[i-1]),m);
+        vector<vector<int>>m(amount+1,vector<int>(coins.size()+1));
+        for(int i=0;i<=coins.size();i++){
+            m[0][i] = 0;
         }
-        count2 = this->rec(coins,i-1,amount,m);
-        if(count1 == -1 && count2 == -1) m[amount][i] = -1;
-        else if(count1 == -1) m[amount][i] =  count2;
-        else if(count2 == -1) m[amount][i] =  count1+1;
-        else m[amount][i] =  min(count1+1,count2);
-        return m[amount][i];
+        for(int i=1;i<=amount;i++){
+            m[i][0] = -1;
+        }
+
+        for(int i=1;i<=amount;i++){
+            for(int j=1;j<=coins.size();j++){
+                int count1 =-1, count2 =-1;
+                //if i chose to use the current element
+                if(i>= coins[j-1]){
+                    count1 = m[i-coins[j-1]][j];
+                }
+                //if i chose to not use current element
+                count2 = m[i][j-1];
+                if(count1 == -1 && count2 ==-1) m[i][j] = -1;
+                else if(count1 == -1) m[i][j] = count2;
+                else if(count2 == -1) m[i][j] = count1+1;
+                else m[i][j] = min(count1+1,count2);
+            }
+        }
+
+        // for(int i=0;i<=amount;i++){
+        //     for(int j=0;j<=coins.size();j++){
+        //         cout<<m[i][j] << " ";
+        //     }
+        //     cout<<endl;
+        // }
+        return m[amount][coins.size()];
     }
+
 };
