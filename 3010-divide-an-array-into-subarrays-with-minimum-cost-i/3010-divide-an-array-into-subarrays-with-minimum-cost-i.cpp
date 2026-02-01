962408e843b3ba5619e19639d6b1c5dd51f7f02b
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-02-01 10:35:49 +0530

    Time: 79 ms (1.51%), Space: 30.5 MB (48.24%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
-        int num1 = nums[1], num2 = nums[2];
+        int minEl = std::min(nums[1],nums[2]);
+        int maxEl = std::max(nums[1],nums[2]);
         for(int i=3;i<nums.size();i++){
-            if(num1 > nums[i]){
-                int storedValue = num1;
-                num1 = nums[i];
-                num2 = storedValue < num2 ? storedValue : num2;
-            }else if(num2 > nums[i]){
-                int storedValue = num2;
-                num2 = nums[i];
-                num1 = storedValue < num1 ? storedValue : num1;                
+            if(nums[i]<maxEl){
+                int storedMinVal = minEl;
+                minEl = std::min(minEl,nums[i]);
+                maxEl = std::max(storedMinVal,nums[i]);
             }
+            cout << minEl << " " << maxEl << endl;
         }
 
+        return maxEl + minEl + nums[0];
 
-        return (nums[0] + num1 + num2);
+        // int num1 = nums[1], num2 = nums[2];
+        // for(int i=3;i<nums.size();i++){
+        //     if(num1 > nums[i]){
+        //         int storedValue = num1;
+        //         num1 = nums[i];
+        //         num2 = storedValue < num2 ? storedValue : num2;
+        //     }else if(num2 > nums[i]){
+        //         int storedValue = num2;
+        //         num2 = nums[i];
+        //         num1 = storedValue < num1 ? storedValue : num1;                
+        //     }
+        // }
+
+
+        // return (nums[0] + num1 + num2);
         
     }
 };
