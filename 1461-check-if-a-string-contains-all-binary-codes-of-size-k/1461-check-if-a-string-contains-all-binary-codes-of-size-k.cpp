f39fd2ca7c2a6215af14b434f095e2fe7754dcbd
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-02-23 22:03:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        unordered_map<string,bool> m;
+        for(int i=0;i+k<s.size();i++){
+            string subString = "";
+            for(int j=i;j<i+k;j++){
+                subString += s[j];
+            }
+
+            if(m.find(subString) == m.end()) m[subString] = true;
+        }
+
+        int possiblities = pow(2,k);
+
+        
+
+        return possiblities <= m.size() ?  true :  false;
+// 
+        // return false;
+        
+    }
+};
