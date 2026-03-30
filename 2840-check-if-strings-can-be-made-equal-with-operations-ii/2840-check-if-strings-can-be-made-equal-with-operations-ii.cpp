a148453c71e55c551fd81a4600ade96ff0cecffc
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-03-30 20:07:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,25 +2,18 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         vector<int> m(52,0);
-        // vector<int> even(26,0);
-
         for(int i=0;i<s1.size();i++){
             if(i&1){
-                //odd index
                 m[(s1[i]-'a')] += 1;
                 m[(s2[i]-'a')] -= 1;
             }else{
-                //even index
                 m[(26+(s1[i]-'a'))] += 1;
                 m[(26+(s2[i]-'a'))] -= 1;
             }
         }
-
         for(auto num : m){
             if(num) return false;
         }
-
         return true;
-        
     }
 };
