da7d68f0d17c7d784cce284c4953ec97c943261a
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-04-05 14:16:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
+        int dx[128] = {0};
+        int dy[128] = {0};
+        dy['U'] =1; dy['D'] = -1;
+        dx['L'] = -1; dx['R'] = 1;
         int x = 0;
         int y = 0;
         for(auto el : moves){
-            switch(el){
-                case 'U':
-                    y += 1;
-                    break;
-                case 'D':
-                    y -= 1;
-                    break;
-                case 'R':
-                    x += 1;
-                    break;
-                case 'L':
-                    x -= 1;
-                    break;
-            }
-
+            x += dx[el];
+            y += dy[el];
         }
 
         if(x==0 && y==0) return true;
