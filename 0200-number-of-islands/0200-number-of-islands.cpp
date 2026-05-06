f72235357590b5ca590a00ebc253c7fad8c524a9
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-05-07 01:37:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<bool>>visited(grid.size(),vector<bool>(grid[0].size()));
         int count = 0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(!visited[i][j] && grid[i][j] == '1'){
-                    this->dfs(grid,visited,i,j);
+                if(grid[i][j] == '1'){
+                    this->dfs(grid,i,j);
                     count++;
                 }
             }
         }
         return count;
-
-        
     }
-    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col){
+    void dfs(vector<vector<char>>& grid, int row, int col){
         if(row <0 || row >= grid.size() || col < 0 || col > grid[0].size()) return;
-        if(!visited[row][col] && grid[row][col] == '1'){
-            visited[row][col] = true;
-            this->dfs(grid,visited,row+1,col);
-            this->dfs(grid,visited,row-1,col);
-            this->dfs(grid,visited,row,col+1);
-            this->dfs(grid,visited,row,col-1);
+        if(grid[row][col] == '1'){
+            // visited[row][col] = true;
+            grid[row][col] = '0';
+            this->dfs(grid,row+1,col);
+            this->dfs(grid,row-1,col);
+            this->dfs(grid,row,col+1);
+            this->dfs(grid,row,col-1);
         }
     }
 };
