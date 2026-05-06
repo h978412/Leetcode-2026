de8266872a10989de4b950de59a4f3e460218949
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-05-07 00:13:32 +0530

    Time: 31 ms (28.15%), Space: 16.7 MB (57.1%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        vector<vector<bool>>visited(grid.size(),vector<bool>(grid[0].size()));
         int count = 0;
-        // vector<vector<bool>(grid[0].size(),false)> visited(grid.size());
-        vector<vector<bool>> visited(grid.size(),vector<bool>(grid[0].size(),false));
         for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                if(visited[i][j] || grid[i][j] == '0') continue;
-                else{
-                    this->dsf(i,j,grid,visited);
+            for(int j=0;j<grid[0].size();j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    this->dfs(grid,visited,i,j);
                     count++;
                 }
             }
         }
         return count;
+
+        
     }
-    void dsf(int i, int j,vector<vector<char>>& grid,vector<vector<bool>>& visited){
-        if(i<0 || i>= grid.size() || j<0 || j >= grid[i].size()) return;
-        if(visited[i][j] || grid[i][j] == '0') return;
-        // cout << i << " " << j << endl;
-        visited[i][j] = true;
-        this->dsf(i+1,j,grid,visited);
-        this->dsf(i-1,j,grid,visited);
-        this->dsf(i,j+1,grid,visited);
-        this->dsf(i,j-1,grid,visited);
+    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col){
+        if(row <0 || row >= grid.size() || col < 0 || col > grid[0].size()) return;
+        if(!visited[row][col] && grid[row][col] == '1'){
+            visited[row][col] = true;
+            this->dfs(grid,visited,row+1,col);
+            this->dfs(grid,visited,row-1,col);
+            this->dfs(grid,visited,row,col+1);
+            this->dfs(grid,visited,row,col-1);
+        }
     }
 };
