4fc014b2169e4a4c7bef1aa0ba5910129d9d1ec3
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-05-07 22:42:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.cpp
+++ 3660-jump-game-ix/3660-jump-game-ix.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    vector<int> maxValue(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> maxArr(n, nums[0]);
-        for (int i = 1; i < nums.size(); i++) {
-            maxArr[i] = max(maxArr[i - 1], nums[i]);
-        }
-        vector<int> ans(n, maxArr[n - 1]);
-        map<int, int> m;
-        m.insert({nums[n - 1], n - 1});
-        ans[n-1] = maxArr[n - 1];
+    static vector<int> maxValue(vector<int>& nums) {
+        const int n=nums.size();
+        vector<int> prefMax(n), sufMin(n);
+        prefMax[0]=nums[0];
+        sufMin[n-1]=nums[n-1];
 
-        for (int i = n - 2; i >= 0; i--) {
-            auto it = m.lower_bound(nums[i]);
-            if (it == m.begin()) {
-                auto it1 = m.lower_bound(maxArr[i]);
-                if (it1 == m.begin()) {
-                    ans[i] = maxArr[i];
-                } else {
-                    it1 = --it1;
-                    ans[i] = ans[m[it1->first]];
-                }
-                m.insert({nums[i], i});
-            } else {
-                it = --it;
-                ans[i] = ans[m[it->first]];
-            }
+        for(int i=1; i<n; i++){
+            const int x=nums[i], y=nums[n-1-i];
+            prefMax[i]=max(prefMax[i-1], x);
+            sufMin[n-1-i]=min(sufMin[n-i], y);
+        }
+        vector<int> ans(n);
+        ans[n-1]=prefMax[n-1];
+        for(int i=n-2; i>=0; i--){
+            if (prefMax[i]>sufMin[i+1]) 
+                ans[i]=max(prefMax[i], ans[i+1]);
+            else ans[i]=prefMax[i];
         }
         return ans;
     }
