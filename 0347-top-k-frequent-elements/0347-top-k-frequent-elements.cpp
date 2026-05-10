3e194787a0352f125d51e59aa032a054bdb9ea6d
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-05-10 14:56:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,7 +4,7 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>m;
         vector<int> ans;
-        for(auto num : nums) m[num] += 1;
+        for(auto& num : nums) m[num] += 1;
         priority_queue<pair<int,int>> pq;
         for(auto const& [num, freq] : m){
             pq.push({freq,num});
