0072b531fd0171c1ec6e1340bd1f002fc5512a60
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-05-10 14:45:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    struct Compare{
-        unordered_map<int,int>& m;
-        Compare(unordered_map<int,int>& mapRef) : m(mapRef){}
-        bool operator()(int a, int b){
-            return m.at(a) > m.at(b);
-        }
-    };
+
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>m;
         for(auto num : nums){
             m[num] += 1;
         }
+        auto comparator = [&](int a, int b){
+            return m[a] > m[b];
+        };
 
-        priority_queue<int,vector<int>,Compare> pq((Compare(m)));
+        priority_queue<int,vector<int>,decltype(comparator)> pq(comparator);
         for(auto const& [key, value] : m){
             pq.push(key);
             if(pq.size() > k) pq.pop();
