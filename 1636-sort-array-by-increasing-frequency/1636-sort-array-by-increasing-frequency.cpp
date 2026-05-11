6e27508359137ddc646070913dfb3c16adaa576c
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-05-12 01:03:16 +0530

    Time: 5 ms (18.63%), Space: 16.7 MB (5.52%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        map<int,int>m;
+        vector<int> ans;
+        for(auto& num : nums){
+            m[num] += 1;
+        }
+        auto comp = [&m](int a, int b){
+            int c = m[a];
+            int d = m[b];
+            if(c == d) return a < b;
+            return c > d;
+        };
+        priority_queue<int,vector<int>,decltype(comp)>pq(comp);
+
+        for(auto& num  : nums){
+            pq.push(num);
+        }
+        // int i = 0;
+        while(!pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+
+        return ans;
+        
+    }
+};
