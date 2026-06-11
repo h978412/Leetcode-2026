3e90b71ff05906424d2f30fe8bd10860a4f0ad85
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-06-11 23:59:23 +0530

    Time: 458 ms (25.51%), Space: 367 MB (25.5%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -7,13 +7,18 @@ public:
             m[edge[0]].push_back(edge[1]);
             m[edge[1]].push_back(edge[0]);
         }
-        int h = this->dfs(1,0,m);
+        int h = this->dfs(1,0,m)-1;
         long long ans = 1;
-        while(--h){
-            ans *= 2;
-            ans %= 1000000007;
+        long long base = 2;
+        int mod = 1000000007;
+        while (h) {
+            if (h & 1)
+                ans = ans * base % mod;
+            base = base * base % mod;
+            h >>= 1;
         }
         return ans;
+        
     }
 
     int dfs(int n, int prv, vector<vector<int>>& m){
