43f730b47e604f025f1a0bf723aa50d5470368e2
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-06-13 00:12:42 +0530

    Time: 305 ms (46.71%), Space: 333 MB (61.18%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    // vector<int>
     int assignEdgeWeights(vector<vector<int>>& edges) {
-        vector<vector<int>>m(edges.size()+2);
-        for(auto edge : edges){
-            m[edge[0]].push_back(edge[1]);
-            m[edge[1]].push_back(edge[0]);
-        }
-        int h = this->dfs(1,0,m)-1;
-        long long ans = 1;
+        vector<vector<int>> tree = this->buildTree(edges);
+        int height = this->dfs(tree,1,0);
+        return this->powOfTwo(height-1);
+
+
+    }
+    int powOfTwo(int n){
         long long base = 2;
         int mod = 1000000007;
-        while (h) {
-            if (h & 1)
-                ans = ans * base % mod;
-            base = base * base % mod;
-            h >>= 1;
+        long long result = 1;
+        while(n){
+            if(n&1) 
+                result = (result*base) % mod;
+            
+            base = (base*base) % mod;
+            n >>= 1;
         }
-        return ans;
-        
+        return result;
     }
-
-    int dfs(int n, int prv, vector<vector<int>>& m){
+    int dfs(vector<vector<int>>& tree, int n, int p){
         int height = -1;
-        for(auto el : m[n]){
-            if(el != prv){
-                height = max(this->dfs(el,n,m),height);
+        for(const auto& el : tree[n]){
+            if(el != p){
+                height = max(height,this->dfs(tree,el,n));
             }
         }
         return height+1;
+    }
+    vector<vector<int>> buildTree(vector<vector<int>>& edges){
+        vector<vector<int>>tree(edges.size()+2);
+        for(const auto& edge : edges){
+            tree[edge[0]].push_back(edge[1]);
+            tree[edge[1]].push_back(edge[0]);
+        }
+
+        return tree;
     }
 };
