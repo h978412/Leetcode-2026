19cd41f9a57ffa31b50c2e2750c54369a56496f9
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-06-28 02:06:56 +0530

    Time: 9 ms (96.15%), Space: 22.6 MB (92.31%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.php
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.php
@@ -7,14 +7,13 @@ class Solution {
      */
     function subarraySum($nums, $k) {
         $sum = 0;
-        $map = [];
+        $map = [0=>1];
         $count = 0;
 
         foreach($nums as $num){
             $sum += $num;
             $target = $sum-$k;
             if(isset($map[$target])) $count += $map[$target];
-            if($target == 0) $count++;
             $map[$sum] = isset($map[$sum])?$map[$sum]+1:1;
         }
 
