6607aea06cbbd52240eabc007f7b119366fc0e05
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-06-28 02:08:17 +0530

    Time: 9 ms (96.15%), Space: 22.9 MB (80.77%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.php
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.php
@@ -1,23 +1,27 @@
 class Solution {
-
-    /**
-     * @param Integer[] $nums
-     * @param Integer $k
-     * @return Integer
-     */
     function subarraySum($nums, $k) {
-        $sum = 0;
-        $map = [0=>1];
         $count = 0;
+        $sum = 0;
+        
+        // 1. Pre-seed the base case to remove the inner if($target == 0) check
+        $map = [0 => 1]; 
 
-        foreach($nums as $num){
+        foreach ($nums as $num) {
             $sum += $num;
-            $target = $sum-$k;
-            if(isset($map[$target])) $count += $map[$target];
-            $map[$sum] = isset($map[$sum])?$map[$sum]+1:1;
+            
+            // 2. Calculate directly in the check
+            if (isset($map[$sum - $k])) {
+                $count += $map[$sum - $k];
+            }
+            
+            // 3. In-place modification instead of ternary reassignment
+            if (isset($map[$sum])) {
+                $map[$sum]++;
+            } else {
+                $map[$sum] = 1;
+            }
         }
 
         return $count;
-        
     }
 }
