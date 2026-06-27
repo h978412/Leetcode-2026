2cfebe6a12cf95032f8e63fa3e7dd6521785429a
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-06-28 02:16:59 +0530

    Time: 8 ms (77.45%), Space: 31.9 MB (59.8%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.php
@@ -0,0 +1,35 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function productExceptSelf($nums) {
+        $isZFound = false;
+        $product = 1;
+        foreach($nums as $num){
+            if($num == 0){
+                if($isZFound) $product = 0;
+                else $isZFound = true;
+            }else{
+                $product *= $num;
+            }
+        }
+        $ans = [];
+        foreach($nums as $num){
+            if($isZFound){
+                if($num){
+                    array_push($ans,0);
+                }else{
+                    array_push($ans,$product);
+                }
+
+            }else{
+                array_push($ans,$product/$num);
+            }
+        }
+
+        return $ans;
+        
+    }
+}
