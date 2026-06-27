b7575cee1fa0e3f6a91cac7e339d493ea6733e1b
Harsh Kumar Agarwal <77280440+h978412@users.noreply.github.com>
2026-06-28 02:19:01 +0530

    Time: 8 ms (77.45%), Space: 32.1 MB (32.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.php
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.php
@@ -11,16 +11,19 @@ class Solution {
             if($num == 0){
                 if($isZFound) $product = 0;
                 else $isZFound = true;
-            }
-            else $product *= $num;
+            } else 
+                $product *= $num;
         }
         $ans = [];
         foreach($nums as $num){
             if($isZFound){
-                if($num) array_push($ans,0);
-                else array_push($ans,$product);
+                if($num) 
+                    array_push($ans,0);
+                else 
+                    array_push($ans,$product);
             }
-            else array_push($ans,$product/$num);
+            else 
+                array_push($ans,$product/$num);
         }
 
         return $ans;
